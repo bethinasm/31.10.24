0814e8a505d7a7229a142149f42bb4724fc8f277
Bethina S.M <bethina.s.moen@gmail.com>
2024-11-01 14:56:48 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -23,10 +23,10 @@ function addFriend(userID) {
 
 
     if (currentUser && friendToAdd) { //sjekker at begge finnes (ikke er null eller undefined)
-        if (!currentUser.friends){
+        if (!currentUser.friends){ // oppretter en tom array for currentUser.friends
             currentUser.friends = [];
         }
-        if(!currentUser.friends.includes(friendToAdd.id)) {
+        if(!currentUser.friends.includes(friendToAdd.id)) { //hvis currentUser.friends array ikke includes friendToAdd
             currentUser.friends.push(friendToAdd.id);
             
             console.log(`${currentUser.username} added ${friendToAdd.username} as a friend`)
